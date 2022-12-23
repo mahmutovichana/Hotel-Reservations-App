4936b933218aa5cd9523798e962e97a3e3efa592
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2022-12-23 14:27:42 +0100

    3. added User.java to domain folder

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -0,0 +1,98 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int role;
+    private String username;
+
+    private String password;
+
+    public User(String firstName, String lastName, String email, int role, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return role == user.role && firstName.equals(user.firstName) && lastName.equals(user.lastName) && Objects.equals(email, user.email) && username.equals(user.username) && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, role, username, password);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
