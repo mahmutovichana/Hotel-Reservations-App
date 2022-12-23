e2cec897d4e0a4028d09b1d58858f5ee518e5bf9
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2022-12-23 14:40:17 +0100

    10. added ReservationDao interface to dao folder

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Reservation;
+
+/**
+ * Dao interface for Reservation domain bean
+ *
+ * @author Hana Mahmutovic
+ */
+public interface ReservationDao extends Dao<Reservation> {
+
+    /* here we insert methods that are special only for that table, the implementation of it we do in SQL Impl file */
+
+}
