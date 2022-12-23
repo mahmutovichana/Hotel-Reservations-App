d0421ddc2829d9ed4bd2270b50e238f196d777a6
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2022-12-23 14:40:55 +0100

    11. added UserDao interface to dao folder

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.User;
+
+/**
+ * Dao interface for User domain bean
+ *
+ * @author Hana Mahmutovic
+ */
+public interface UserDao extends Dao<User> {
+
+    /* here we insert methods that are special only for that table, the implementation of it we do in SQL Impl file */
+
+}
