001530140ad259c58b67a271a6de9439bff77d81
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2022-12-23 14:44:29 +0100

    12. added HotelDaoSQLImpl.java in dao folder with its connection to the database of the project

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/HotelDaoSQLImpl.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Hotel;
+
+import java.io.FileReader;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class HotelDaoSQLImpl implements HotelDao {
+
+    private Connection connection;
+
+    public HotelDaoSQLImpl(){
+        try{
+            FileReader reader = new FileReader("src/main/resources/db.properties");
+            Properties p = new Properties();
+            p.load(reader);
+            String s1 = p.getProperty("username"), s2 = p.getProperty("password"), s3 = p.getProperty("server");
+            this.connection = DriverManager.getConnection(s3, s1, s2);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
