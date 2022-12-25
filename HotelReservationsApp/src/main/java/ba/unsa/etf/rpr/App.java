703a08c165c465e3531f63c9a509692d666e5755
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2022-12-25 20:24:13 +0100

    52. changed the App file to load the application and all the windows

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/App.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,41 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        // Here you can work with args - command line parameters
+        Application.launch(args);
     }
-}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/Main.fxml")));
+        primaryStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
+        /*
+            primaryStage.setTitle("Hana Avis");
+            primaryStage.setWidth(300); primaryStage.setHeight(300);
+            primaryStage.setScene(new Scene(root, 300, 275));
+            - we don't technically need this because we set our scene to be transparent, but I left it, so I can
+            remember how the syntax is to set the title in the title bar, same thing for the dimension of the app window
+        */
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/stylesheets/styles.css");
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }}
