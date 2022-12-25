23da422100f7801daa515343f6257522848cf96b
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2022-12-25 20:20:54 +0100

    47. added SignUpController.java to controllers

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/SignUpController.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+    public TextField name;
+    public Label badName;
+    public TextField surname;
+    public TextField email;
+    public TextField username;
+    public PasswordField password;
+    public Button signUpButton;
+}
