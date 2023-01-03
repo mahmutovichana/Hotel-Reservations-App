b09cd43317b6a7e0a55ac8139eb64c9cdf6d9a5a
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-03 18:46:29 +0100

    88. added the AboutUsPageController

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/AboutUsPageController.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AboutUsPageController {
+
+    @FXML
+    private ImageView goBack;
+
+}
