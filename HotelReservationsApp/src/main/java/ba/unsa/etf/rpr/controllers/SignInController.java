a1030c6d171b5ce4d846ffaa6c424b01cd81fa32
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-03 09:04:47 +0100

    53. added the SignInController with its attributes

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/SignInController.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/SignInController.java
@@ -1,11 +1,39 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.User;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class SignInController {
-    public TextField email;
-    public PasswordField password;
+    public TextField usernameField;
+    public PasswordField passwordField;
     public Button signInButton;
+    @FXML
+    public Label badUsernameIN;
+    @FXML
+    public Label badPasswordIN;
+    @FXML
+    public Label errorLabel;
+
+
 }
