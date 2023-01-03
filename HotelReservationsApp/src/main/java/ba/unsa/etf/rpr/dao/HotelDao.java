0efd952c28beb836cbb4bc5eca9134045e31b2da
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-03 17:24:34 +0100

    72. added the declaration for getAllNames in the HotelDao

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/HotelDao.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/HotelDao.java
@@ -1,6 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Hotel;
+import java.io.FileReader;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Dao interface for Hotel domain bean
@@ -11,5 +16,6 @@ public interface HotelDao extends Dao<Hotel>{
 
     /* here we insert methods that are special only for that table, the implementation of it we do in SQL Impl file */
 
+    public List<String> getAllNames();
 
 }
