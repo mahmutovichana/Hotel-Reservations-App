47c7900eb29b1e1597ffbe723879e37ee0ec40e3
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-03 18:46:55 +0100

    89. added the initialize method to the AboutUsPageController

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/AboutUsPageController.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/AboutUsPageController.java
@@ -15,4 +15,23 @@ public class AboutUsPageController {
     @FXML
     private ImageView goBack;
 
+    public void initialize() {
+        goBack.setOnMouseClicked(event -> {
+            // Close the current window
+            Stage stage = (Stage) goBack.getScene().getWindow();
+            stage.close();
+            // Open the previous window
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/homePage/HomePage.fxml"));
+                Parent root = fxmlLoader.load();
+                Stage homeStage = new Stage();
+                homeStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
+                homeStage.setScene(new Scene(root));
+                homeStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
 }
