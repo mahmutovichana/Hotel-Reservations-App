05d1fdb64901d430529d98be4cb0b61a0bf1c03d
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:33:37 +0100

    140. added the MyProfilePageController, felt needed might delete later

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/MyProfilePageController.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MyProfilePageController {
+
+    @FXML
+    private ImageView goBack;
+
+    public void initialize() {
+        goBack.setOnMouseClicked(event -> {
+            // Close the current window
+            Stage stage = (Stage) goBack.getScene().getWindow();
+            stage.close();
+            // Open the previous window
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/homePage/HomePage.fxml"));
+                Parent root = fxmlLoader.load();
+                Stage homeStage = new Stage();
+                homeStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
+                homeStage.setScene(new Scene(root));
+                homeStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
