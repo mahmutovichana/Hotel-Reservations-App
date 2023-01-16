f0cbceef0505572f2c5cb567a41acb18b81b72f5
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:30:15 +0100

    138. changed the sql implementation full for the rooms table (refactoring)

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLImpl.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLImpl.java
@@ -1,138 +1,71 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Room;
+import ba.unsa.etf.rpr.exceptions.HotelException;
 
-import java.io.FileReader;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class RoomDaoSQLImpl implements RoomDao {
+/**
+ * MySQL Implementation of DAO
+ * @author Hana Mahmutović
+ */
+public class RoomDaoSQLImpl extends AbstractDao<Room> implements RoomDao {
 
-    private Connection connection;
+    private static RoomDaoSQLImpl instance = null;
+    private RoomDaoSQLImpl() {
+        super("ROOMS");
+    }
 
-    public RoomDaoSQLImpl() {
+    /**
+     * @author Hana Mahmutović
+     * @return QuoteDaoSQLImpl
+     * We don't need more than one object for CRUD operations on table 'quotes' so we will use Singleton
+     * This method will call private constructor in instance==null and then return that instance
+     */
+    public static RoomDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new RoomDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
+    }
+
+    @Override
+    public Room row2object(ResultSet rs) throws HotelException{
         try {
-            FileReader reader = new FileReader("src/main/resources/db.properties");
-            Properties p = new Properties();
-            p.load(reader);
-            String s1 = p.getProperty("username"), s2 = p.getProperty("password"), s3 = p.getProperty("server");
-            this.connection = DriverManager.getConnection(s3, s1, s2);
+            Room room = new Room();
+            room.setId(rs.getInt("id"));
+            room.setType(rs.getString("type"));
+            room.setCapacity(rs.getInt("capacity"));
+            room.setHasAirConditioning(rs.getInt("hasAirConditioning"));
+            room.setStatus(rs.getInt("status"));
+            room.setHotelId(DaoFactory.hotelDao().getById(rs.getInt("hotel_id")));
+            room.setPrice(rs.getDouble("price"));
+            return room;
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new HotelException(e.getMessage(), e);
         }
     }
 
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
     @Override
-    public Room getById(int id) {
-        String query = "SELECT * FROM ROOMS WHERE room_id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()){ // result set is iterator.
-                Room room = new Room();
-                room.setRoomId(rs.getInt("room_id"));
-                room.setType(rs.getString("type"));
-                room.setCapacity(rs.getInt("capacity"));
-                room.setHasAirConditioning(rs.getInt("hasAirConditioning"));
-                room.setStatus(rs.getInt("status"));
-                room.setPrice(rs.getDouble("price"));
-                room.setHotelId(new HotelDaoSQLImpl().getById(rs.getInt(1)));
-                rs.close();
-                return room;
-            }else{
-                return null; // if there is no elements in the result set return null
-            }
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return null;
+    public Map<String, Object> object2row(Room object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("type", object.getType());
+        item.put("capacity", object.getCapacity());
+        item.put("hasAirConditioning",object.getHasAirConditioning());
+        item.put("status",object.getStatus());
+        item.put("hotel_id",object.getHotelId().getId());
+        item.put("price", object.getPrice());
+        return item;
     }
-
-    @Override
-    public Room getByUsername(String username) {
-        return null;
-    }
-
-    @Override
-    public Room add(Room room) {
-        String insert = "INSERT INTO ROOMS(type,capacity,hasAirConditioning,status,hotel_id,price) VALUES(?,?,?,?,?,?)";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, room.getType());
-            stmt.setInt(2,room.getCapacity());
-            stmt.setInt(3,room.getHasAirConditioning());
-            stmt.setInt(4,room.getStatus());
-            stmt.setInt(5,room.getHotelId().getHotelId());
-            stmt.setDouble(6,room.getPrice());
-            stmt.executeUpdate();
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next(); // we know that there is one key
-            room.setRoomId(rs.getInt(1)); //set username to return it back
-            return room;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public Room update(Room room) {
-        String insert = "UPDATE ROOMS SET type = ?, capacity = ?, hasAirConditioning = ?, status = ?, hotel_id = ?, price = ? WHERE room_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, room.getType());
-            stmt.setObject(2, room.getCapacity());
-            stmt.setObject(3, room.getHasAirConditioning());
-            stmt.setObject(4, room.getStatus());
-            stmt.setObject(5, room.getHotelId());
-            stmt.setObject(6, room.getPrice());
-            stmt.setObject(7, room.getRoomId());
-            stmt.executeUpdate();
-            return room;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public void delete(Room room) {
-        String insert = "DELETE FROM ROOMS WHERE room_id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, room.getRoomId());
-            stmt.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Room> getAll() {
-        String query = "SELECT * FROM ROOMS";
-        List<Room> rooms = new ArrayList<>();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                Room room = new Room();
-                room.setRoomId(rs.getInt("room_id"));
-                room.setType(rs.getString("type"));
-                room.setCapacity(rs.getInt("capacity"));
-                room.setHasAirConditioning(rs.getInt("hasAirConditioning"));
-                room.setStatus(rs.getInt("status"));
-                room.setHotelId(new HotelDaoSQLImpl().getById(rs.getInt("hotel_id")));
-                room.setPrice(rs.getDouble("price"));
-                rooms.add(room);
-            }
-            rs.close();
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return rooms;
-    }
-
 }
