265e25137e2a68704d1ace8862d246ff652c482e
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:41:20 +0100

    153. added the throws Exception part to the Dao interface

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,53 +1,46 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.HotelException;
+
 import java.util.List;
 
 /**
  * Root interface for all DAO classes
  *
- * @author Hana Mahmutovic
+ * @author Hana Mahmutović
  */
 public interface Dao<T> {
 
     /**
-     * get entity from database based on id
-     *
+     * get entity from database base on ID
      * @param id primary key of entity
      * @return Entity from database
      */
-    T getById(int id);
-
-    /**
-     * get entity from database based on username
-     *
-     * @param username primary key of entity
-     * @return Entity from database
-     */
-    T getByUsername(String username);
+    T getById(int id) throws HotelException;
 
     /**
      * Saves entity into database
-     * @param object bean for saving to database
+     * @param item bean for saving to database
      * @return saved item with id field populated
      */
-    T add(T object);
+    T add(T item) throws HotelException;
 
     /**
      * Fully updates entity in database based on id (primary) match.
-     * @param object - bean to be updated. id must be populated
+     * @param item - bean to be updated. id must be populated
      * @return updated version of bean
      */
-    T update(T object);
+    T update(T item) throws HotelException;
 
     /**
      * Hard delete of item from database with given id
-     * @param object - primary key of entity
+     * @param id - primary key of entity
      */
-    void delete(T object);
+    void delete(int id) throws HotelException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
      */
-    List<T> getAll();
+    List<T> getAll() throws HotelException;
 }
