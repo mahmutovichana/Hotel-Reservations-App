c5d9861d5066836695b80d5d79e18ef59db3f014
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:57:25 +0100

    160. added the utility method for executing any query in the AbstractDao

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -153,4 +153,30 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
     }
 
 
+    /**
+     * Utility method for executing any kind of query
+     * @param query - SQL query
+     * @param params - params for query
+     * @return List of objects from database
+     * @throws HotelException in case of error with db
+     */
+    public List<T> executeQuery(String query, Object[] params) throws HotelException{
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            if (params != null){
+                for(int i = 1; i <= params.length; i++){
+                    stmt.setObject(i, params[i-1]);
+                }
+            }
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<T> resultList = new ArrayList<>();
+            while (rs.next()) {
+                resultList.add(row2object(rs));
+            }
+            return resultList;
+        } catch (SQLException e) {
+            throw new HotelException(e.getMessage(), e);
+        }
+    }
+
 }
