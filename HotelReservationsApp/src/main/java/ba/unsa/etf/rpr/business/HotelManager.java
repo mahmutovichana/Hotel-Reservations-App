6dac62cafe72cd47b10e0c38dcb87a5b853e9a37
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:35:05 +0100

    142. added the HotelManager to the business logic

---
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/business/HotelManager.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Hotel;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+
+import java.util.List;
+
+public class HotelManager {
+    public void delete(int hotelId) throws HotelException {
+        try {
+            DaoFactory.hotelDao().delete(hotelId);
+        } catch (HotelException e) {
+            if (e.getMessage().contains("FOREIGN KEY")) {
+                throw new HotelException("NO");
+            }
+            throw e;
+        }
+
+    }
+
+    public Hotel add(Hotel h) throws HotelException {
+        return DaoFactory.hotelDao().add(h);
+    }
+
+    public Hotel getById(int id) throws HotelException {
+        return DaoFactory.hotelDao().getById(id);
+    }
+
+    public Hotel update(Hotel h) throws HotelException {
+        return DaoFactory.hotelDao().update(h);
+    }
+
+    public List<Hotel> getAll() throws HotelException {
+        return DaoFactory.hotelDao().getAll();
+    }
+}
