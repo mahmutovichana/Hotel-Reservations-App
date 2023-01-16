bf6f1c91bbfbd46ea23c165fe62b3933e2b707d8
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:37:44 +0100

    146. changed the sql implementation full for the hotels table (refactoring)

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/HotelDaoSQLImpl.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/HotelDaoSQLImpl.java
@@ -1,174 +1,75 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Hotel;
-import java.io.FileReader;
-import java.sql.*;
-import java.util.ArrayList;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+
+import java.sql.ResultSet;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class HotelDaoSQLImpl implements HotelDao {
+/**
+ * MySQL Implementation of DAO
+ * @author Hana Mahmutović
+ */
+public class HotelDaoSQLImpl extends AbstractDao<Hotel> implements HotelDao {
 
-    private Connection connection;
+    private static HotelDaoSQLImpl instance = null;
+    private HotelDaoSQLImpl() {
+        super("HOTELS");
+    }
 
-    public HotelDaoSQLImpl(){
-        try{
-            FileReader reader = new FileReader("src/main/resources/db.properties");
-            Properties p = new Properties();
-            p.load(reader);
-            String s1 = p.getProperty("username"), s2 = p.getProperty("password"), s3 = p.getProperty("server");
-            this.connection = DriverManager.getConnection(s3, s1, s2);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    /**
+     * @author Hana Mahmutović
+     * @return QuoteDaoSQLImpl
+     * We don't need more than one object for CRUD operations on table 'quotes' so we will use Singleton
+     * This method will call private constructor in instance==null and then return that instance
+     */
+    public static HotelDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new HotelDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
     }
 
     @Override
-    public Hotel getByUsername(String username) {
-        String query = "SELECT * FROM HOTELS WHERE name = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1, username);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()){ // result set is iterator.
-                Hotel hotel = new Hotel();
-                hotel.setHotelId(rs.getInt("hotel_id"));
-                hotel.setName(rs.getString("name"));
-                hotel.setZipCode(rs.getInt("zipCode"));
-                hotel.setCity(rs.getString("city"));
-                hotel.setCountry(rs.getString("country"));
-                hotel.setStarRating(rs.getInt("starRating"));
-                rs.close();
-                return hotel;
-            }else{
-                return null; // if there is no elements in the result set return null
-            }
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return null;
-    }
-
-    public Hotel getById(int hotelId){
-        String query = "SELECT * FROM HOTELS WHERE hotel_id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, hotelId);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()){ // result set is iterator.
-                Hotel hotel = new Hotel();
-                hotel.setHotelId(rs.getInt("hotel_id"));
-                hotel.setName(rs.getString("name"));
-                hotel.setZipCode(rs.getInt("zipCode"));
-                hotel.setCity(rs.getString("city"));
-                hotel.setCountry(rs.getString("country"));
-                hotel.setStarRating(rs.getInt("starRating"));
-                rs.close();
-                return hotel;
-            }else{
-                return null; // if there is no elements in the result set return null
-            }
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return null;
-    }
-
-    @Override
-    public Hotel add(Hotel hotel) {
-        String insert = "INSERT INTO HOTELS(name,zipCode,city,country,starRating) VALUES(?,?,?,?,?)";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1,hotel.getName());
-            stmt.setInt(2,hotel.getZipCode());
-            stmt.setString(3,hotel.getCity());
-            stmt.setString(4,hotel.getCountry());
-            stmt.setInt(5,hotel.getStarRating());
-            stmt.executeUpdate();
-            ResultSet rs = stmt.getGeneratedKeys();
-            while(rs.next()){
-                hotel.setName(rs.getString(2)); //set username to return it back
-            }
-            //rs.next(); // we know that there is one key
-
+    public Hotel row2object(ResultSet rs) throws HotelException{
+        try {
+            Hotel hotel = new Hotel();
+            hotel.setId(rs.getInt("id"));
+            hotel.setName(rs.getString("name"));
+            hotel.setZipCode(rs.getInt("zipCode"));
+            hotel.setCity(rs.getString("city"));
+            hotel.setCountry(rs.getString("country"));
+            hotel.setStarRating(rs.getInt("starRating"));
             return hotel;
-        }catch (SQLException e){
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new HotelException(e.getMessage(), e);
         }
-        return null;
+    }
+
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
+    @Override
+    public Map<String, Object> object2row(Hotel object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("name", object.getName());
+        item.put("zipCode", object.getZipCode());
+        item.put("city", object.getCity());
+        item.put("country", object.getCountry());
+        item.put("starRating",object.getStarRating());
+        return item;
     }
 
     @Override
-    public Hotel update(Hotel hotel) {
-        String insert = "UPDATE HOTELS SET zipCode = ?, city = ?, country = ?, starRating = ? WHERE name = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, hotel.getZipCode());
-            stmt.setObject(2, hotel.getCity());
-            stmt.setObject(3, hotel.getCountry());
-            stmt.setObject(4, hotel.getStarRating());
-            stmt.setObject(5, hotel.getName());
-            stmt.executeUpdate();
-            return hotel;
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public void delete(Hotel hotel) {
-        String insert = "DELETE FROM HOTELS WHERE hotel_id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, hotel.getHotelId());
-            stmt.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Hotel> getAll() {
-        String query = "SELECT * FROM HOTELS";
-        List<Hotel> hotels = new ArrayList<>();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                Hotel hotel = new Hotel();
-                hotel.setHotelId(rs.getInt("hotel_id"));
-                hotel.setName(rs.getString("name"));
-                hotel.setZipCode(rs.getInt("zipCode"));
-                hotel.setCity(rs.getString("city"));
-                hotel.setCountry(rs.getString("country"));
-                hotel.setStarRating(rs.getInt("starRating"));
-                hotels.add(hotel);
-            }
-            rs.close();
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return hotels;
-    }
-
     public List<String> getAllNames() {
-        String query = "SELECT * FROM HOTELS";
-        List<String> hotelNames = new ArrayList<>();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                Hotel hotel = new Hotel();
-                hotel.setHotelId(rs.getInt("hotel_id"));
-                hotel.setName(rs.getString("name"));
-                hotelNames.add(hotel.getName());
-            }
-            rs.close();
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return hotelNames;
+        return null;
     }
-
 }
