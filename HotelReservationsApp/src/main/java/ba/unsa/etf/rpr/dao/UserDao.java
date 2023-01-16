50ed51546d9ab6483c0191dcac41dee8338b9e36
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-01-16 18:26:28 +0100

    132. able to throw an exception for the UserDao

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.HotelException;
 
 /**
  * Dao interface for User domain bean
@@ -8,6 +9,7 @@ import ba.unsa.etf.rpr.domain.User;
  * @author Hana Mahmutovic
  */
 public interface UserDao extends Dao<User> {
+    User findUsername(String usernameField) throws HotelException;
 
     /* here we insert methods that are special only for that table, the implementation of it we do in SQL Impl file */
 
