e39607f1302a5186c171dc3f4fab9b744697dd3b
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-07 15:20:29 +0100

    210. added the definition of the totalIncome function

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Reservation;
 
+import java.sql.SQLException;
+
 /**
  * Dao interface for Reservation domain bean
  *
@@ -10,5 +12,5 @@ import ba.unsa.etf.rpr.domain.Reservation;
 public interface ReservationDao extends Dao<Reservation> {
 
     /* here we insert methods that are special only for that table, the implementation of it we do in SQL Impl file */
-
+    public int totalIncome() throws SQLException;
 }
