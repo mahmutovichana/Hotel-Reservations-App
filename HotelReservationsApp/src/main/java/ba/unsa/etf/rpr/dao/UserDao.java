6e046fd04c4762e9e4b1c411edefc70d717b9543
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-07 15:06:45 +0100

    205. added a totalUsers function definition

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -3,6 +3,8 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 
+import java.sql.SQLException;
+
 /**
  * Dao interface for User domain bean
  *
@@ -12,5 +14,7 @@ public interface UserDao extends Dao<User> {
     User findUsername(String usernameField) throws HotelException;
 
     /* here we insert methods that are special only for that table, the implementation of it we do in SQL Impl file */
+
+    public int totalUsers() throws SQLException;
 
 }
