4e612cc27d9fed189d3f55612fc95e9162361e2b
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-07 15:03:52 +0100

    200. added some functions to the controller

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/MyProfilePageController.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/MyProfilePageController.java
@@ -1,36 +1,142 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class MyProfilePageController {
 
+    public Pane root;
+    public Button aboutUsButton;
+    public ImageView logOutButton;
+    public Button reservationsButton;
+    private User user;
+    public MyProfilePageController(User u){this.user=u;}
     @FXML
-    private ImageView goBack;
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @FXML
+    public User getUser() {
+        return user;
+    }
+    @FXML
+    public Label usernameLabel = new Label();
+
+    @FXML
+    public Label firstNameLabel = new Label();
+
+    @FXML
+    public Label lastNameLabel = new Label();
+
+    @FXML
+    public Label emailLabel = new Label();
+
+    @FXML
+    public Button closeButton;
 
     public void initialize() {
-        goBack.setOnMouseClicked(event -> {
+
+        logOutButton.setOnMouseClicked(event -> logOut());
+
+        firstNameLabel.setText(user.getFirstName());
+        lastNameLabel.setText(user.getLastName());
+        emailLabel.setText(user.getEmail());
+        usernameLabel.setText(user.getUsername());
+
+        closeButton.setOnAction(this::closeButtonAction);
+        closeButton.setOnMouseEntered(this::closeButtonMouseEntered);
+        closeButton.setOnMouseExited(this::closeButtonMouseExited);
+
+        aboutUsButton.setOnAction(event -> {
             // Close the current window
-            Stage stage = (Stage) goBack.getScene().getWindow();
+            Stage stage = (Stage) aboutUsButton.getScene().getWindow();
             stage.close();
-            // Open the previous window
+            // Open the about us page window
             try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/homePage/HomePage.fxml"));
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/homePage/AboutUsPage.fxml"));
                 Parent root = fxmlLoader.load();
-                Stage homeStage = new Stage();
-                homeStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
-                homeStage.setScene(new Scene(root));
-                homeStage.show();
+                Stage aboutUsStage = new Stage();
+                aboutUsStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
+                aboutUsStage.initStyle(StageStyle.TRANSPARENT);
+                Scene scene = new Scene(root);
+                scene.setFill(Color.TRANSPARENT);
+                aboutUsStage.setScene(scene);
+                aboutUsStage.show();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+
+        reservationsButton.setOnAction(event -> {
+            // Close the current window
+            Stage stage = (Stage) reservationsButton.getScene().getWindow();
+            stage.close();
+            // Open the "my profile" page window
+            try {
+                Stage myProfileStage = new Stage();
+                myProfileStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
+                myProfileStage.initStyle(StageStyle.TRANSPARENT);
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/homePage/ListOfReservationsPage.fxml"));
+                ListOfReservationsPageController controller = new ListOfReservationsPageController(user);
+                fxmlLoader.setController(controller);
+                Parent root = fxmlLoader.load();
+
+                Scene scene = new Scene(root);
+                scene.setFill(Color.TRANSPARENT);
+                myProfileStage.setScene(scene);
+                myProfileStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void logOut() {
+        // Close the current window
+        Stage stage = (Stage) logOutButton.getScene().getWindow();
+        stage.close();
+        // Open the login window
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main/Main.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage loginStage = new Stage();
+            loginStage.getIcons().add(new Image("images/HanaAvisTransLogoBlue.png"));
+            loginStage.setScene(new Scene(root, Color.TRANSPARENT));
+            loginStage.initStyle(StageStyle.TRANSPARENT);
+            loginStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void closeButtonAction(ActionEvent event) {
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
+    @FXML
+    private void closeButtonMouseExited(MouseEvent mouseEvent) {
+        closeButton.getStyleClass().remove("closeButtonWhenHovered");
+    }
+    @FXML
+    private void closeButtonMouseEntered(MouseEvent mouseEvent) {
+        closeButton.getStyleClass().add("closeButtonStyle");
+        closeButton.getStyleClass().add("closeButtonWhenHovered");
     }
 }
