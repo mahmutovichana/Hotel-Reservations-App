b5293b3d396c1f23206d41dfea5766ae678f3973
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-09 16:12:27 +0100

    225. changed some methods

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
@@ -6,7 +6,16 @@ import ba.unsa.etf.rpr.exceptions.HotelException;
 
 import java.util.List;
 
+/**
+ * The type Reservation manager.
+ */
 public class ReservationManager {
+    /**
+     * Delete.
+     *
+     * @param ReservationId the reservation id
+     * @throws HotelException the hotel exception
+     */
     public void delete(int ReservationId) throws HotelException {
         try {
             DaoFactory.reservationDao().delete(ReservationId);
@@ -19,18 +28,45 @@ public class ReservationManager {
 
     }
 
+    /**
+     * Add reservation.
+     *
+     * @param r the r
+     * @return the reservation
+     * @throws HotelException the hotel exception
+     */
     public Reservation add(Reservation r) throws HotelException {
         return DaoFactory.reservationDao().add(r);
     }
 
+    /**
+     * Gets by id.
+     *
+     * @param id the id
+     * @return the by id
+     * @throws HotelException the hotel exception
+     */
     public Reservation getById(int id) throws HotelException {
         return DaoFactory.reservationDao().getById(id);
     }
 
+    /**
+     * Update reservation.
+     *
+     * @param r the r
+     * @return the reservation
+     * @throws HotelException the hotel exception
+     */
     public Reservation update(Reservation r) throws HotelException {
         return DaoFactory.reservationDao().update(r);
     }
 
+    /**
+     * Gets all.
+     *
+     * @return the all
+     * @throws HotelException the hotel exception
+     */
     public List<Reservation> getAll() throws HotelException {
         return DaoFactory.reservationDao().getAll();
     }
