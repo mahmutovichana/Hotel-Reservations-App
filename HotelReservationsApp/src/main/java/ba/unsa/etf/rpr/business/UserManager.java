4db64ca28b11d9401687831f8586d18847e3f496
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-10 20:47:24 +0100

    235. added a hash function so the password will be hashed when inputed

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -4,6 +4,8 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public class UserManager {
@@ -36,5 +38,20 @@ public class UserManager {
     }
     public User findUsername(String usernameField) throws HotelException{
         return DaoFactory.userDao().findUsername(usernameField);
+    }
+
+    private static final String HASHING_ALGORITHM = "SHA-256";
+
+    public static String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance(HASHING_ALGORITHM);
+        messageDigest.update(password.getBytes());
+
+        byte[] hashedPassword = messageDigest.digest();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : hashedPassword) {
+            stringBuilder.append(String.format("%02x", b));
+        }
+        return stringBuilder.toString();
     }
 }
