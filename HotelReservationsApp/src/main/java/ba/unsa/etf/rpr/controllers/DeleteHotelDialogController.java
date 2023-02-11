27a7182d00f5a94d839b9581d212e769879c1b9f
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-11 22:59:55 +0100

    244. sredila brisanje hotela

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/DeleteHotelDialogController.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/DeleteHotelDialogController.java
@@ -1,64 +1,73 @@
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.business.HotelManager;
 import ba.unsa.etf.rpr.domain.Hotel;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
 
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * The type Delete hotel dialog controller.
+ */
 public class DeleteHotelDialogController {
 
+    private final HotelManager h = new HotelManager();
     @FXML
     private ComboBox<Hotel> hotelsComboBox;
 
+    /**
+     * The Delete button.
+     */
     @FXML
     public Button deleteButton;
 
+    /**
+     * The Cancel button.
+     */
     @FXML
     public Button cancelButton;
 
     private Hotel selectedHotel;
     private boolean okClicked = false;
 
+    /**
+     * Sets hotels.
+     *
+     * @param hotels the hotels
+     */
     public void setHotels(List<Hotel> hotels) {
         hotelsComboBox.getItems().addAll(hotels);
     }
-    /*public void setHotels(List<Hotel> hotels) {
-        hotelsComboBox.setItems(FXCollections.observableArrayList(hotels));
-        hotelsComboBox.setConverter(new StringConverter<Hotel>() {
-            @Override
-            public String toString(Hotel object) {
-                return object.getName();
-            }
-            @Override
-            public Hotel fromString(String string) {
-                return null;
-            }
-        });
-    }*/
-    public boolean isOkClicked() {
-        return okClicked;
-    }
 
+    /**
+     * Sets ok clicked.
+     *
+     * @return the ok clicked
+     */
+    public boolean isOkClicked() { return okClicked; }
+
+    /**
+     * Gets selected hotel.
+     *
+     * @return the selected hotel
+     */
     public Hotel getSelectedHotel() {
         return selectedHotel;
     }
 
     @FXML
-    private void handleDeleteHotel() throws SQLException {
+    private void handleDeleteHotel(){
         selectedHotel = hotelsComboBox.getSelectionModel().getSelectedItem();
         if (selectedHotel != null) {
             okClicked = true;
             Stage stage = (Stage) deleteButton.getScene().getWindow();
             try {
-                int totalHotels = DaoFactory.hotelDao().totalHotels();
+                h.totalHotels();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -66,6 +75,11 @@ public class DeleteHotelDialogController {
         }
     }
 
+    /**
+     * Cancel delete.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void cancelDelete(ActionEvent actionEvent) {
         Button cancelButton = (Button) actionEvent.getSource();
