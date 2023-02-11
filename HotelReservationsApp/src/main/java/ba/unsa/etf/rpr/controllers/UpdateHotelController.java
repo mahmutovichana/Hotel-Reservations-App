cf7c740433092ee0d9eccfa554ee883a9c36d025
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-11 21:51:04 +0100

    241. changed some functions in the controller

--- HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/UpdateHotelController.java
+++ HotelReservationsApp/src/main/java/ba/unsa/etf/rpr/controllers/UpdateHotelController.java
@@ -1,103 +1,7 @@
-/*package ba.unsa.etf.rpr.controllers;
-
-import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.domain.Hotel;
-import ba.unsa.etf.rpr.exceptions.HotelException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import javax.print.DocFlavor;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.ResourceBundle;
-
-public class UpdateHotelController {
-    public Button cancelButton;
-    public Button saveButton;
-    public ComboBox hotelComboBox;
-    public TextField starRatingField;
-    public TextField countryField;
-    public TextField cityField;
-    public TextField zipCodeField;
-    public TextField nameField;
-
-    private Hotel hotel = new Hotel();
-    private boolean okClicked = false;
-    private ObservableList<Hotel> hotels;
-
-    @FXML
-    private void handleOk() throws HotelException {
-        int selectedIndex = hotelComboBox.getSelectionModel().getSelectedIndex();
-        Hotel selectedHotel = hotels.get(selectedIndex);
-        selectedHotel.setName(nameField.getText());
-        selectedHotel.setZipCode(Integer.parseInt(zipCodeField.getText()));
-        selectedHotel.setCity(cityField.getText());
-        selectedHotel.setCountry(countryField.getText());
-        selectedHotel.setStarRating(Integer.parseInt(starRatingField.getText()));
-        DaoFactory.hotelDao().update(selectedHotel);
-        okClicked = true;
-        Stage stage = (Stage) nameField.getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    private void handleCancel() {
-        Stage stage = (Stage) nameField.getScene().getWindow();
-        stage.close();
-    }
-
-    public void initialize() throws HotelException {
-        List<Hotel> allHotels = DaoFactory.hotelDao().fetchHotels();
-        hotels = FXCollections.observableArrayList(allHotels);
-        hotelComboBox.setItems(hotels);
-        hotelComboBox.getSelectionModel().selectFirst();
-        hotelComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                nameField.setText(newValue.getName());
-                zipCodeField.setText(String.valueOf(newValue.getZipCode()));
-                cityField.setText(newValue.getCity());
-                countryField.setText(newValue.getCountry());
-                starRatingField.setText(String.valueOf(newValue.getStarRating()));
-            }
-        });
-    }
-
-    @FXML
-    private void saveHotel() throws HotelException {
-            Hotel selectedHotel = (Hotel) hotelComboBox.getSelectionModel().getSelectedItem();
-            selectedHotel.setName(nameField.getText());
-            selectedHotel.setZipCode(Integer.parseInt(zipCodeField.getText()));
-            selectedHotel.setCity(cityField.getText());
-            selectedHotel.setCountry(countryField.getText());
-            selectedHotel.setStarRating(Integer.parseInt(starRatingField.getText()));
-            DaoFactory.hotelDao().update(selectedHotel);
-            okClicked = true;
-            ((Stage) saveButton.getScene().getWindow()).close();
-        }
-
-        @FXML
-        private void cancelHotel() {
-            ((Stage) cancelButton.getScene().getWindow()).close();
-        }
-
-
-        public void updateHotel(ActionEvent actionEvent) {
-    }
-
-    public void cancel(ActionEvent actionEvent) {
-    }
-}
-*/
-
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.business.HotelManager;
+import ba.unsa.etf.rpr.dao.AbstractDao;
 import ba.unsa.etf.rpr.domain.Hotel;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 import javafx.collections.FXCollections;
@@ -108,32 +12,64 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Update hotel controller.
+ */
 public class UpdateHotelController {
+    /**
+     * The Cancel button.
+     */
     public Button cancelButton;
+    /**
+     * The Save button.
+     */
     public Button saveButton;
+    /**
+     * The Hotel combo box.
+     */
     public ComboBox<Hotel> hotelComboBox;
+    /**
+     * The Star rating field.
+     */
     public TextField starRatingField;
+    /**
+     * The Country field.
+     */
     public TextField countryField;
+    /**
+     * The City field.
+     */
     public TextField cityField;
+    /**
+     * The Zip code field.
+     */
     public TextField zipCodeField;
+    /**
+     * The Name field.
+     */
     public TextField nameField;
 
     private boolean okClicked = false;
-    private ObservableList<Hotel> hotels;
+    private List<Hotel> hotels = new ArrayList<>();
     private Hotel hotel;
+
+    private final HotelManager h = new HotelManager();
 
     @FXML
     private void handleOk() throws HotelException {
-        int selectedIndex = hotelComboBox.getSelectionModel().getSelectedIndex();
-        Hotel selectedHotel = hotels.get(selectedIndex);
+        Hotel selectedHotel = hotelComboBox.getSelectionModel().getSelectedItem();
         selectedHotel.setName(nameField.getText());
         selectedHotel.setZipCode(Integer.parseInt(zipCodeField.getText()));
         selectedHotel.setCity(cityField.getText());
         selectedHotel.setCountry(countryField.getText());
         selectedHotel.setStarRating(Integer.parseInt(starRatingField.getText()));
-        DaoFactory.hotelDao().update(selectedHotel);
+        h.update(selectedHotel);
+        System.out.println(selectedHotel.getName());
+        System.out.println(h.getById(8).toString());
         okClicked = true;
         Stage stage = (Stage) nameField.getScene().getWindow();
         stage.close();
@@ -145,11 +81,18 @@ public class UpdateHotelController {
         stage.close();
     }
 
-    public void initialize() throws HotelException {
-        List<Hotel> allHotels = DaoFactory.hotelDao().fetchHotels();
+    /**
+     * Initialize.
+     *
+     * @throws HotelException the hotel exception
+     */
+    public void initialize() throws HotelException, SQLException {
+        if(AbstractDao.getConnection().isClosed()) System.out.println("zatvorena");
+        List<Hotel> allHotels = h.getAll();
         hotels = FXCollections.observableArrayList(allHotels);
-        hotelComboBox.setItems(hotels);
+        hotelComboBox.setItems((ObservableList<Hotel>) hotels);
         hotelComboBox.getSelectionModel().selectFirst();
+
         hotelComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
                 nameField.setText(newValue.getName());
@@ -161,20 +104,22 @@ public class UpdateHotelController {
         });
     }
 
+    /**
+     * Is ok clicked boolean.
+     *
+     * @return the boolean
+     */
     public boolean isOkClicked() {
         return okClicked;
     }
 
+    /**
+     * Sets hotel.
+     *
+     * @param hotel the hotel
+     */
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
-    }
-
-    public void setOkClicked(boolean okClicked) {
-        this.okClicked = okClicked;
-    }
-
-    public void setHotels(ObservableList<Hotel> hotels) {
-        this.hotels = hotels;
     }
 }
 
