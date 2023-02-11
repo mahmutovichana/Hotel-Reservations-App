f32c0820d016c816185ac685d1d182d7f0a2e5fc
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-11 10:55:16 +0100

    238. added 2 more JUnit tests

---
+++ HotelReservationsApp/src/test/java/ba/unsa/etf/rpr/dao/UserDaoTest.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDaoTest {
+    @Test
+    public void testUserConstructor() {
+        User user = new User("John", "Doe", "john.doe@example.com", 1, "johndoe", "password");
+
+        assertEquals("John", user.getFirstName());
+        assertEquals("Doe", user.getLastName());
+        assertEquals("john.doe@example.com", user.getEmail());
+        assertEquals(1, user.getRole());
+        assertEquals("johndoe", user.getUsername());
+        assertEquals("password", user.getPassword());
+    }
+
+    @Test
+    public void testUserSettersAndGetters() {
+        User user = new User();
+        user.setFirstName("Jane");
+        user.setLastName("Doe");
+        user.setEmail("jane.doe@example.com");
+        user.setRole(2);
+        user.setUsername("janedoe");
+        user.setPassword("password");
+
+        assertEquals("Jane", user.getFirstName());
+        assertEquals("Doe", user.getLastName());
+        assertEquals("jane.doe@example.com", user.getEmail());
+        assertEquals(2, user.getRole());
+        assertEquals("janedoe", user.getUsername());
+        assertEquals("password", user.getPassword());
+    }
+}
