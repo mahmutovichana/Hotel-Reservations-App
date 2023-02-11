09f52bd9a31f8afeb2adeda36e69053c8ebbfde4
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-02-11 04:25:40 +0100

    237. added 2 mock tests

--- HotelReservationsApp/src/test/java/ba/unsa/etf/rpr/dao/HotelDaoTest.java
+++ HotelReservationsApp/src/test/java/ba/unsa/etf/rpr/dao/HotelDaoTest.java
@@ -2,46 +2,49 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.business.HotelManager;
 import ba.unsa.etf.rpr.domain.Hotel;
+import ba.unsa.etf.rpr.exceptions.HotelException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
-public class HotelDaoTest {
+class HotelDaoTest {
 
     private HotelManager hotelManager;
+    @Mock
     private HotelDao hotelDao;
 
+    public Hotel hotel = new Hotel("Mocky", 10001, "New York", "USA", 5);
+
     @BeforeEach
-    void setUp() {
-        hotelDao = mock(HotelDao.class);
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
         hotelManager = new HotelManager();
     }
 
     @Test
     void hotelConstructorTest() {
-        Hotel hotel = new Hotel("Hilton", 10001, "New York", "USA", 5);
-        assertEquals("Hilton", hotel.getName());
+        assertEquals("Mocky", hotel.getName());
         assertEquals(10001, hotel.getZipCode());
         assertEquals("New York", hotel.getCity());
         assertEquals("USA", hotel.getCountry());
         assertEquals(5, hotel.getStarRating());
     }
 
-
     @Test
-    void addTest() throws Exception {
-        Hotel hotel = new Hotel("Hilton", 10001, "New York", "USA", 5);
-        when(hotelDao.add(hotel)).thenReturn(hotel);
-        assertEquals(hotel, hotelManager.add(hotel));
+    public void addTest() throws HotelException {
+        hotelDao.add(hotel);
+        verify(hotelDao).add(hotel);
     }
+
 
     @Test
     void updateTest() throws Exception {
-        Hotel hotel = new Hotel("Hilton", 10001, "New York", "USA", 5);
-        when(hotelDao.update(hotel)).thenReturn(hotel);
-        assertEquals(hotel, hotelManager.update(hotel));
+        hotel.setName("Hotel Mocky");
+        hotelDao.update(hotel);
+        verify(hotelDao).update(hotel);
     }
 }
